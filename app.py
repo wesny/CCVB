82a0ded2c351fcf4d5979e814ad32fab1222ce60
Sweyn Venderbush <sweyn3@yahoo.com>
2014-03-27 12:28:18 -0400

    Add setup files

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, session, render_template, request, redirect, url_for
-
+from py import *
 
 app = Flask(__name__)
+app.config.from_object('py.config')
 
-app.secret_key = "wrluwvlwrnmnlqewrpqwreoghclvkjfn"
+env = app.jinja_env
+env.line_statement_prefix = '='
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
 
 if __name__ == '__main__':
     app.debug = True;
