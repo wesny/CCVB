a44b94a3c24a973b10bfc4583d55abce88c83c24
David Cahn <cahnda@gmail.com>
2014-04-02 20:04:09 -0400

    Struggling with Twitter API, code

--- py/Twitter.py
+++ py/Twitter.py
@@ -1,5 +1,11 @@
 import tweepy
 
-public_tweets = tweepy.api.public_timeline()
+Consumer_Key = 'jkITE2vTsnbZbO4isffog'
+Consumer_Secret = 'VAUmkrC3Tjj1OgVMLrtkSoNtvD7k4SZ6PhG0elBWHUw'
+Access_Token = '1531685562-9OgrUddG7L5bP9zUJETYf4sfABGC5U1cI2rxXeo'
+Access_Token_Secret = '5v43cr0rax5fLhYKF67lAHlobNVjQqVncCm4GgtEFyl8o'
+usrname = 'twitter'
+
+public_tweets = tweepy.API.get_user(usrname)
 for tweet in public_tweets:
     print tweet.text
