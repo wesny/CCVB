e41b8ff0ad6be64f19d0c6866c8daebc7fa26469
David Cahn <cahnda@gmail.com>
2014-04-02 20:10:32 -0400

    Added STTwitter library

---
+++ py/STTwitter/STTwitter/NSError+STTwitter.m
@@ -0,0 +1,61 @@
+//
+//  NSError+STTwitter.m
+//  STTwitterDemoOSX
+//
+//  Created by Nicolas Seriot on 19/03/14.
+//  Copyright (c) 2014 Nicolas Seriot. All rights reserved.
+//
+
+#import "NSError+STTwitter.h"
+
+@implementation NSError (STTwitter)
+
++ (NSError *)st_twitterErrorFromResponseData:(NSData *)responseData
+                             responseHeaders:(NSDictionary *)responseHeaders
+                             underlyingError:(NSError *)underlyingError {
+    
+    NSError *jsonError = nil;
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableLeaves error:&jsonError];
+    
+    NSString *message = nil;
+    NSInteger code = 0;
+    
+    if([json isKindOfClass:[NSDictionary class]]) {
+        // assume format: {"errors":[{"message":"Sorry, that page does not exist","code":34}]}
+        
+        id errors = [json valueForKey:@"errors"];
+        if([errors isKindOfClass:[NSArray class]] && [(NSArray *)errors count] > 0) {
+            NSDictionary *errorDictionary = [errors lastObject];
+            if([errorDictionary isKindOfClass:[NSDictionary class]]) {
+                message = errorDictionary[@"message"];
+                code = [[errorDictionary valueForKey:@"code"] integerValue];
+            }
+        } else if([errors isKindOfClass:[NSString class]]) {
+            // assume format {errors = "Screen name can't be blank";}
+            message = errors;
+        }
+    }
+    
+    if(message) {
+        NSString *rateLimitLimit = [responseHeaders valueForKey:@"x-rate-limit-limit"];
+        NSString *rateLimitRemaining = [responseHeaders valueForKey:@"x-rate-limit-remaining"];
+        NSString *rateLimitReset = [responseHeaders valueForKey:@"x-rate-limit-reset"];
+        
+        NSDate *rateLimitResetDate = rateLimitReset ? [NSDate dateWithTimeIntervalSince1970:[rateLimitReset doubleValue]] : nil;
+        
+        NSMutableDictionary *md = [NSMutableDictionary dictionary];
+        md[NSLocalizedDescriptionKey] = message;
+        if(underlyingError) md[NSUnderlyingErrorKey] = underlyingError;
+        if(rateLimitLimit) md[kSTTwitterRateLimitLimit] = rateLimitLimit;
+        if(rateLimitRemaining) md[kSTTwitterRateLimitRemaining] = rateLimitRemaining;
+        if(rateLimitResetDate) md[kSTTwitterRateLimitResetDate] = rateLimitResetDate;
+        
+        NSDictionary *userInfo = [NSDictionary dictionaryWithDictionary:md];
+        
+        return [NSError errorWithDomain:kSTTwitterTwitterErrorDomain code:code userInfo:userInfo];
+    }
+    
+    return nil;
+}
+
+@end
