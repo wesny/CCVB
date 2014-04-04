20cb5278228fb17cee13a45f9bf10548018cbf39
cahnda <cahnda@gmail.com>
2014-04-03 22:24:25 -0400

    Successful user search using twitter api

--- py/Twitter.py
+++ py/Twitter.py
@@ -7,9 +7,21 @@ consumer_secret = 'VAUmkrC3Tjj1OgVMLrtkSoNtvD7k4SZ6PhG0elBWHUw'
 client = Client(consumer_key, consumer_secret)
 
 # Pretty print of tweet payload
-tweet = client.request('https://api.twitter.com/1.1/statuses/show.json?id=316683059296624640')
-print json.dumps(tweet, sort_keys=True, indent=4, separators=(',', ':'))
+def tweet_payload ():
+    tweet = client.request('https://api.twitter.com/1.1/statuses/show.json?id=316683059296624640')
+    #print json.dumps(tweet, sort_keys=True, indent=4, separators=(',', ':'))
 
 # Show rate limit status for this application
-status = client.rate_limit_status()
-print status['resources']['search']
+def rate_limit():
+    status = client.rate_limit_status()
+    #print status['resources']['search']
+
+# Find a user and get info on him/her
+def findUser (username):
+    #url = 'https://api.twitter.com/1/users/show.json?screen_name=%s&include_entities=true' % (username)
+    url = 'https://api.twitter.com/1.1/users/lookup.json?screen_name=%s' % (username)
+    user = client.request(url)
+    print json.dumps(user, sort_keys=True, indent=4, separators=(',', ':'))
+
+if __name__ == '__main__':
+    print findUser ('nikhilgoya_l')
