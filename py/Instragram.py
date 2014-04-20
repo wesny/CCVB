39601db8602bba1fd5e32b079a2c86658e9ab615
David Cahn <cahnda@gmail.com>
2014-04-20 18:08:11 -0400

    get user id using json

--- py/Instragram.py
+++ py/Instragram.py
@@ -14,16 +14,23 @@ count = 1
 #api = InstagramAPI(client_id="CLIENT_ID", client_secret='CLIENT_SECRET')
 api = InstagramAPI(access_token=access_token)
 
-def get_User_Timeline (username):
-    user =  api.user_search(username, 1)[0]
-        
-#    api.user(user_id) = userID
-    
 
-    #url = ("https://api.instagram.com/v1/users/search?q=%s&count=1&access_token=%s") % (username,access_token)
-    #response = urlopen(url)
-   # print json.dumps(response, sort_keys=True, indent=4, separators=(',', ':'))
-   
+def get_User_Timeline (username):
+    #find user_id
+    #user =  api.user_search(username, 1)[0]    
+    url = ("https://api.instagram.com/v1/users/search?q=%s&access_token=1267672900.1fb234f.ab78269e1b754039b0e900a966e14fdb") %(username)
+    response = urlopen(url)
+    json_raw = response.read()
+    json_data = json.loads(json_raw)
+    user_id = json_data["data"][0]["id"]
+
+    #find user
+
+    url = ("https://api.instagram.com/v1/users/%s") %(user_id)
+    response = urlopen(url)
+    json_raw = response.read()
+    json_data = json.loads(json_raw)
+    print json_data
 
 if __name__ == '__main__':
-     user = get_User_Timeline ('sophieheinberg')
+     user = get_User_Timeline ('DavidCahn')
