1966942dd830d381398ae75e13ce507f0f3f4941
David Cahn <cahnda@gmail.com>
2014-04-23 23:30:36 -0400

    Acceessed Instragram Media

--- py/Instragram.py
+++ py/Instragram.py
@@ -15,7 +15,7 @@ count = 1
 api = InstagramAPI(access_token=access_token)
 
 
-def get_User_Timeline (username):
+def get_User_ID (username):
     #find user_id
     #user =  api.user_search(username, 1)[0]    
     url = ("https://api.instagram.com/v1/users/search?q=%s&access_token=1267672900.1fb234f.ab78269e1b754039b0e900a966e14fdb") %(username)
@@ -24,6 +24,8 @@ def get_User_Timeline (username):
     json_data = json.loads(json_raw)
     user_id = json_data["data"][0]["id"]
 
+def get_User_Data (username):
+    user_id = get_User_ID (username)
     #find user
     #doing it myself because the library is aweful...
     url = ("https://api.instagram.com/v1/users/%s/?access_token=1267672900.1fb234f.ab78269e1b754039b0e900a966e14fdb") %(user_id)
@@ -39,12 +41,28 @@ def get_User_Timeline (username):
     data ["full_name"] = full_name
     data ["total_media"] = total_media
     data ["followed_by"] = followed_by
-   
+    try:
+        media = get_media (user_id)
+    except: 
+        media = []
+    data ["media"] = media
     return data
 
-#def get_media (username):
-    #generic social media url:https://api.instagram.com/v1/users/3/media/recent/?access_token=1267672900.f59def8.c04a11eb198a426eb24ed18378ed4eab
+def get_media (user_id):
+    url = ("https://api.instagram.com/v1/users/%s/media/recent/?access_token=%s") % (user_id,access_token)
+    response = urlopen(url)
+    json_raw = response.read()
+    json_data = json.loads(json_raw)["data"][0]
+    for x in json_data:
+       # print x
+        for x in json_data["user"]:
+            print x
+        for x in json_data["likes"]:
+            print x
+        for x in json_data["comments"][0]:
+            print x
 
 if __name__ == '__main__':
-    data = get_User_Timeline ('jennamarbles')
-    print data
+  #  data = get_User_Timeline ('jennamarbles')
+    media = get_media(6717525)
+
