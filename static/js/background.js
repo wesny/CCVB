02b88a4084e595dd6130c77072a08b99fedc4cf3
cburke496 <cburke496@gmail.com>
2014-05-21 18:16:09 -0400

    Made randomized network background

--- static/js/background.js
+++ static/js/background.js
@@ -5,46 +5,93 @@ var makeBackground = function() {
     width = parseInt(document.width);
     height = parseInt(document.height);
     
-    var rects = s.getElementsByTagName('rect');
-    var len = rects.length
+    var circles = s.getElementsByTagName('circle');
+    var lines = s.getElementsByTagName('line');
+    var len = circles.length
+    
+    for(var i = 0; i < len; i++) {
+	circles[0].parentNode.removeChild(circles[0]);
+    }
+    len = lines.length;
+    for(var i = 0; i < len; i++) {
+	lines[0].parentNode.removeChild(lines[0]);
+    }
+
+    var newCircles = [];
+    
+    for(var i = 0; i < 100; i++) {
+	var randpixel = document.createElementNS("http://www.w3.org/2000/svg","circle");
+	var dimension = Math.random() * 2 + 5;
+	randpixel.setAttribute('cx',Math.random() * width * 1.5 - width * .25);
+	randpixel.setAttribute('cy',Math.random() * height * 1.5 - height * .25);
+	randpixel.setAttribute('r',dimension);
+	randpixel.setAttribute('fill','#6a55ff');
+	
+	newCircles.push(randpixel);
+	
+	var randpixel2 = document.createElementNS("http://www.w3.org/2000/svg","circle");
+	var dimension2 = Math.random() * 2 + 5;
+	randpixel2.setAttribute('cx',Math.random() * width * 1.5 - width * .25);
+	randpixel2.setAttribute('cy',Math.random() * height * 1.5 - height * .25);
+	randpixel2.setAttribute('r',dimension2);
+	randpixel2.setAttribute('fill','#5a70ff');
+
+	newCircles.push(randpixel2);
+	
+	var randpixel3 = document.createElementNS("http://www.w3.org/2000/svg","circle");
+	var dimension3 = Math.random() * 2 + 5;
+	randpixel3.setAttribute('cx',Math.random() * width * 1.5 - width * .25);
+	randpixel3.setAttribute('cy',Math.random() * height * 1.5 - height * .25);
+	randpixel3.setAttribute('r',dimension3);
+	randpixel3.setAttribute('fill','#5e86ff');
+
+	newCircles.push(randpixel3);
+    }
+    
+
+    for(var i = 0; i < newCircles.length; i++){
+	var xcoord = newCircles[i].getAttribute('cx');
+	var ycoord = newCircles[i].getAttribute('cy');
+	var dists = _.map(newCircles, function(d,i) {
+	    return [Math.sqrt(Math.pow(xcoord - d.getAttribute('cx'),2) +
+			     Math.pow(ycoord - d.getAttribute('cy'),2)),i];
+	    });
+
+	var neighbors = _.filter(dists, function(d) {
+	    return d[0] < 200 && d[1] != i;});
+
+
+	var skipRate = 1 / j;
+
+	for(var j = 0; j < neighbors.length && j < 4; j++) {
+	    if(j != 0 && Math.random() > skipRate) {
+		continue;
+	    }
+	    
+	    var newline = document.createElementNS("http://www.w3.org/2000/svg","line");
+	    newline.setAttribute('x1',xcoord);
+	    newline.setAttribute('y1',ycoord);
+	    newline.setAttribute('x2',newCircles[neighbors[j][1]].getAttribute('cx'));
+	    newline.setAttribute('y2',newCircles[neighbors[j][1]].getAttribute('cy'));
+	  
+	    newline.setAttribute('stroke-width',1);
+	    newline.setAttribute('stroke','#8888ff');
+	    s.appendChild(newline);
+	}
+    }
+
+    for(var i = 0; i < newCircles.length; i++) {
+	s.appendChild(newCircles[i]);
+    }
+    
     var leftSide = document.getElementById('leftSide'); 
     var rightSide = document.getElementById('rightSide');
-    for(var i = 0; i < len; i++) {
-	rects[0].parentNode.removeChild(rects[0]);
-    }
-    
-    for(var i = 0; i < 150; i++) {
-	var randpixel = document.createElementNS("http://www.w3.org/2000/svg","rect");
-	var dimension = Math.random() * 10 + 1;
-	randpixel.setAttribute('x',Math.random() * width);
-	randpixel.setAttribute('y',Math.random() * height);
-	randpixel.setAttribute('width',dimension);
-	randpixel.setAttribute('height',dimension);
-	randpixel.setAttribute('fill','#7aff85');
-	s.appendChild(randpixel);
-	
-	var randpixel2 = document.createElementNS("http://www.w3.org/2000/svg","rect");
-	var dimension2 = Math.random() * 10 + 1;
-	randpixel2.setAttribute('x',Math.random() * width);
-	randpixel2.setAttribute('y',Math.random() * height);
-	randpixel2.setAttribute('width',dimension2);
-	randpixel2.setAttribute('height',dimension2);
-	randpixel2.setAttribute('fill','#baffc0');
-	s.appendChild(randpixel2);
-	
-	var randpixel3 = document.createElementNS("http://www.w3.org/2000/svg","rect");
-	var dimension3 = Math.random() * 10 + 1;
-	randpixel3.setAttribute('x',Math.random() * width);
-	randpixel3.setAttribute('y',Math.random() * height);
-	randpixel3.setAttribute('width',dimension3);
-	randpixel3.setAttribute('height',dimension3);
-	randpixel3.setAttribute('fill','#9effa6');
-	s.appendChild(randpixel3);
-    }
-    
+    var topBox = document.getElementById('topBox');
+
     if (leftSide != null) {
 	s.appendChild(leftSide);
 	s.appendChild(rightSide);
+	s.appendChild(topBox);
     }
    
     s.appendChild(foreign);
--- static/js/index.js
+++ static/js/index.js
@@ -9,6 +9,18 @@ title.setAttribute('class',"center");
 foreign.insertBefore(title, fullDiv);
 
 
+var topBox = document.createElementNS("http://www.w3.org/2000/svg","rect");
+topBox.setAttribute('x', "33%");
+topBox.setAttribute('y', 0);
+topBox.setAttribute('width', "34%");
+topBox.setAttribute('height',75);
+topBox.setAttribute('rx', 10);
+topBox.setAttribute('ry', 10);
+topBox.setAttribute('fill', '#eeeeff');
+topBox.setAttribute('stroke', '#d0d0ff');
+topBox.setAttribute('id','topBox');
+s.appendChild(topBox);
+
 var leftSide = document.createElementNS("http://www.w3.org/2000/svg","rect");
 leftSide.setAttribute('x', "10%");
 leftSide.setAttribute('y', 100);
@@ -16,7 +28,8 @@ leftSide.setAttribute('width', "30%");
 leftSide.setAttribute('height',500);
 leftSide.setAttribute('rx', 10);
 leftSide.setAttribute('ry', 10);
-leftSide.setAttribute('fill', '#eeffee');
+leftSide.setAttribute('fill', '#eeeeff');
+leftSide.setAttribute('stroke', '#d0d0ff');
 leftSide.setAttribute('id','leftSide');
 s.appendChild(leftSide);
 
@@ -27,7 +40,8 @@ rightSide.setAttribute('width', "30%");
 rightSide.setAttribute('height', 500);
 rightSide.setAttribute('rx', 10);
 rightSide.setAttribute('ry', 10);
-rightSide.setAttribute('fill', '#eeffee');
+rightSide.setAttribute('fill', '#eeeeff');
+rightSide.setAttribute('stroke', '#d0d0ff');
 rightSide.setAttribute('id','rightSide');
 s.appendChild(rightSide);
 
