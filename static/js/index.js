ce493a2d5e0926d29b569ad631e8256aad51af65
Christopher Burke <cburke496@gmail.com>
2014-05-29 13:11:43 -0400

    Black and white background; beginning to make site scalable

--- static/js/index.js
+++ static/js/index.js
@@ -4,12 +4,12 @@ var fullDiv = document.getElementById("full-div");
 var leftDiv = document.getElementById("left-div");
 var rightDiv = document.getElementById("right-div");
 var foreign = document.getElementById("foreign");
-
+var width = document.body.clientWidth;
 
 var topBox = document.createElementNS("http://www.w3.org/2000/svg","rect");
-topBox.setAttribute('x', "33%");
-topBox.setAttribute('y', 0);
-topBox.setAttribute('width', "34%");
+topBox.setAttribute('x', width / 2 - 207);
+topBox.setAttribute('y', '15px');
+topBox.setAttribute('width', "414px");
 topBox.setAttribute('height',75);
 topBox.setAttribute('rx', 10);
 topBox.setAttribute('ry', 10);
@@ -19,9 +19,9 @@ topBox.setAttribute('id','topBox');
 s.appendChild(topBox);
 
 var leftSide = document.createElementNS("http://www.w3.org/2000/svg","rect");
-leftSide.setAttribute('x', "10%");
-leftSide.setAttribute('y', 100);
-leftSide.setAttribute('width', "30%");
+leftSide.setAttribute('x', width / 4 - 200.5);
+leftSide.setAttribute('y', 120);
+leftSide.setAttribute('width', "401px");
 leftSide.setAttribute('height',500);
 leftSide.setAttribute('rx', 10);
 leftSide.setAttribute('ry', 10);
@@ -31,9 +31,9 @@ leftSide.setAttribute('id','leftSide');
 s.appendChild(leftSide);
 
 var rightSide = document.createElementNS("http://www.w3.org/2000/svg","rect");
-rightSide.setAttribute('x', "60%");
-rightSide.setAttribute('y', 100);
-rightSide.setAttribute('width', "30%");
+rightSide.setAttribute('x', width * 3 / 4 - 200.5);
+rightSide.setAttribute('y', 120);
+rightSide.setAttribute('width', "401px");
 rightSide.setAttribute('height', 500);
 rightSide.setAttribute('rx', 10);
 rightSide.setAttribute('ry', 10);
