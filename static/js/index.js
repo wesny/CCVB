0312852fe90862edf2edb98ccea761234b970810
cburke496 <cburke496@gmail.com>
2014-05-30 19:23:34 -0400

    Moved boxes up and made fading work regardless of the position of the mouse

--- static/js/index.js
+++ static/js/index.js
@@ -22,7 +22,7 @@ s.appendChild(topBox);
 
 var leftSide = document.createElementNS("http://www.w3.org/2000/svg","rect");
 leftSide.setAttribute('x', width / 4 - 200.5);
-leftSide.setAttribute('y', 120);
+leftSide.setAttribute('y', 90);
 leftSide.setAttribute('width', "401px");
 leftSide.setAttribute('height',500);
 leftSide.setAttribute('rx', 10);
@@ -34,7 +34,7 @@ s.appendChild(leftSide);
 
 var rightSide = document.createElementNS("http://www.w3.org/2000/svg","rect");
 rightSide.setAttribute('x', width * 3 / 4 - 200.5);
-rightSide.setAttribute('y', 120);
+rightSide.setAttribute('y', 90);
 rightSide.setAttribute('width', "401px");
 rightSide.setAttribute('height', 500);
 rightSide.setAttribute('rx', 10);
@@ -45,20 +45,22 @@ rightSide.setAttribute('id','rightSide');
 s.appendChild(rightSide);
 
 
-topDiv.addEventListener('mouseover', function(e) {
-    d3.selectAll('#left-div').transition().duration(350).style('opacity',1);
-    d3.selectAll('#right-div').transition().duration(350).style('opacity',1);
-});
 
-rightDiv.addEventListener('mouseover', function(e) {
-    d3.selectAll('#left-div').transition().duration(350).style('opacity',0.2);
-    d3.selectAll('#right-div').transition().duration(350).style('opacity',1);
+document.addEventListener('mousemove', function(e) {
+    if(e.y < 90) {
+	d3.selectAll('#left-div').transition().duration(350).style('opacity',1);
+	d3.selectAll('#right-div').transition().duration(350).style('opacity',1);
+    } else {
+	if(e.x < document.body.clientWidth / 2) {
+	    d3.selectAll('#left-div').transition().duration(350).style('opacity',1);
+	    d3.selectAll('#right-div').transition().duration(350).style('opacity',0.2);
+	} else {
+	    d3.selectAll('#left-div').transition().duration(350).style('opacity',0.2);
+	    d3.selectAll('#right-div').transition().duration(350).style('opacity',1); 
+	}
+    }
 });
-
-leftDiv.addEventListener('mouseover', function(e) {
-    d3.selectAll('#left-div').transition().duration(350).style('opacity',1);
-    d3.selectAll('#right-div').transition().duration(350).style('opacity',0.2);
-});
+	    
 
 
 
